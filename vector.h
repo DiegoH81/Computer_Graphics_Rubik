22642e6b2bb88fc68f0a0a2b8b50a336a12c026a
JCGabr <cornejocastrojosegabriel@gmail.com>
2026-05-15 08:02:17 -0500

    refactor(vector): add implementation vector.cpp

--- vector.h
+++ vector.h
@@ -8,62 +8,27 @@ class Vector3
 public:
     float x, y, z;
 
-    Vector3(const float& in_x = 0.0f, const float& in_y = 0.0f, const float& in_z = 0.0f):
-        x(in_x), y(in_y), z(in_z)
-    {}
+    Vector3(const float& in_x = 0.0f, const float& in_y = 0.0f, const float& in_z = 0.0f);
 
-    Vector3 operator + (const Vector3& other) const
-    {
-        return Vector3(x + other.x, y + other.y, z + other.z);
-    }
+    Vector3 operator + (const Vector3& other) const;
 
     Vector3 operator - () const
     {
         return Vector3(-x, -y, -z);
     }
 
-    Vector3 operator - (const Vector3& other) const
-    {
-        return Vector3(x - other.x, y - other.y, z - other.z);
-    }
+    Vector3 operator - (const Vector3& other) const;
 
-    Vector3 operator / (const float& div) const
-    {
-        if (div < 1e-8f)
-            return Vector3(0.0f, 0.0f, 0.0f);
+    Vector3 operator / (const float& div) const;
 
-        return Vector3(x / div, y / div, z / div);
-    }
-
-    Vector3& operator = (const Vector3& other)
-    {
-        x = other.x;
-        y = other.y;
-        z = other.z;
-
-        return *this;
-    }
+    Vector3& operator = (const Vector3& other);
 };
 
-Vector3 cross (const Vector3& v1, const Vector3& v2)
-{
-    float new_x = v1.y * v2.z - v2.y * v1.z;
-    float new_y = - (v1.x * v2.z - v2.x * v1.z);
-    float new_z = v1.x * v2.y - v2.x * v1.y;
+Vector3 cross (const Vector3& v1, const Vector3& v2);
 
-    return Vector3(new_x, new_y, new_z);
-}
+float vec_length(const Vector3& in_vec);
 
-float vec_length(const Vector3& in_vec)
-{
-    return std::sqrt(in_vec.x * in_vec.x + in_vec.y * in_vec.y + in_vec.z * in_vec.z);
-}
-
-Vector3 normalize (const Vector3& in_vec)
-{
-    float l = vec_length(in_vec);
-    return (in_vec / l);
-}
+Vector3 normalize (const Vector3& in_vec);
 
 using Point3 = Vector3;
 
