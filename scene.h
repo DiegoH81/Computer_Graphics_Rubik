387712a79ba493967a05bf1a8c2948ab8386ca87
GebUCSP <147185580+GebUCSP@users.noreply.github.com>
2026-05-15 09:34:46 -0500

    refactor(scene): add implementation scene.cpp

--- scene.h
+++ scene.h
@@ -22,102 +22,23 @@ public:
 
     Shape* shape;
     
-    SceneNode(int in_id, Shape* in_shape = nullptr) :
-        id(in_id), shape(in_shape),
-        public_transform(), private_transform(),
-        parent_transform()
-    { }
+    SceneNode(int in_id, Shape* in_shape = nullptr);
 
-    void draw(ShaderList& shaders, const Matrix_4& in_parent)
-    {
-        parent_transform = in_parent;
-        if (shape)
-            shape->draw(shaders, parent_transform * private_transform);
-        
-        for (auto &child : children)
-            child->draw(shaders, parent_transform * public_transform);
-    }
+    void draw(ShaderList& shaders, const Matrix_4& in_parent);
 
-    Point3 get_center()
-    {
-        Point3 to_return;
+    Point3 get_center();
 
-        if (shape)
-            to_return = parent_transform * private_transform * shape->center;
+    void add_children(SceneNode* in_children);
 
-        return to_return;
-    }
-
-    void add_children(SceneNode* in_children)
-    {
-        children.push_back(in_children);
-    }
-
-    void traslate(const Vector3& in_m, bool is_public)
-    {
-        private_transform.traslate(in_m);
-        if (is_public)
-            public_transform.traslate(in_m);
-    }
+    void traslate(const Vector3& in_m, bool is_public);
     
-    void rotate_x_global(float in_angle, bool is_public)
-    {
-        private_transform.rotate_x(in_angle);
-
-        if (is_public)
-            public_transform.rotate_x(in_angle);
-    }
-    void rotate_y_global(float in_angle, bool is_public)
-    {
-        private_transform.rotate_y(in_angle);
-
-        if (is_public)
-            public_transform.rotate_y(in_angle);
-    }
-    void rotate_z_global(float in_angle, bool is_public)
-    {
-        private_transform.rotate_z(in_angle);
-
-        if (is_public)
-            public_transform.rotate_z(in_angle);
-    }
-    void rotate_x_local(float in_angle, bool is_public)
-    {
-        Matrix_4 R;
-        R.rotate_x(in_angle);
-
-        private_transform = private_transform * R;
-        if (is_public)
-            public_transform = public_transform * R;
-    }
-    void rotate_y_local(float in_angle, bool is_public)
-    {
-        Matrix_4 R;
-        R.rotate_y(in_angle);
-
-        private_transform = private_transform * R;
-        if (is_public)
-            public_transform = public_transform * R;
-    }
-    void rotate_z_local(float in_angle, bool is_public)
-    {
-        Matrix_4 R;
-        R.rotate_z(in_angle);
-
-        private_transform = private_transform * R;
-        if (is_public)
-            public_transform = public_transform * R;
-    }
-    
-    void scale(const Vector3& in_s, bool is_public)
-    {
-        Matrix_4 E;
-        E.scale(in_s);
-
-        private_transform = private_transform * E;
-        if (is_public)
-            public_transform  = public_transform * E;
-    }
+    void rotate_x_global(float in_angle, bool is_public);
+    void rotate_y_global(float in_angle, bool is_public);
+    void rotate_z_global(float in_angle, bool is_public);
+    void rotate_x_local(float in_angle, bool is_public);
+    void rotate_y_local(float in_angle, bool is_public);
+    void rotate_z_local(float in_angle, bool is_public);
+    void scale(const Vector3& in_s, bool is_public);
 
 private:
     std::vector<SceneNode*> children;
